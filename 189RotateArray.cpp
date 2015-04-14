5f7f94425d7d5d795d52dff089eb6997f01ef3f1
Zhanben <ncuzhangben@qq.com>
2015-04-14 19:06:53 +0800

    Create 189RotateArray.cpp

---
+++ 189RotateArray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(int nums[], int n, int k) {
+    //方法一：not in place.  
+    k = k % n;  
+    if (k == 0) return;  
+    int *temp = new int[n];  
+    memcpy(temp, nums+(n-k), sizeof(int)*k);  
+    memcpy(temp+k, nums, sizeof(int)*(n-k));  
+    memcpy(nums, temp, sizeof(int)*n);  
+    delete[] temp; 
+    }
+};
